f481f2204a895505212a14c36c6bedfe0271caf9
Andrii Rodionov <Andrii.Rodionov@gmail.com>
2016-10-02 21:07:26 +0300

    TemperatureSeries template project commit

--- src/main/java/ua/edu/ucu/tempseries/TemperatureSeriesAnalysis.java
+++ src/main/java/ua/edu/ucu/tempseries/TemperatureSeriesAnalysis.java
@@ -1,52 +1,52 @@
 package ua.edu.ucu.tempseries;
 
-public class TemperatureSeriesAnalysis {    
+public class TemperatureSeriesAnalysis {
 
     public TemperatureSeriesAnalysis() {
-        
+
     }
-    
+
     public TemperatureSeriesAnalysis(double[] temperatureSeries) {
-        
+
     }
-    
-    public double average(){        
-        return 0;
-    }    
-    
-    public double deviation(){
-        return 0;
+
+    public double average() {
+        return -1;
     }
-    
-    public double min(){
+
+    public double deviation() {
         return 0;
     }
-     
-    public double max(){
+
+    public double min() {
         return 0;
     }
-    
-    public double findTempClosestToZero(){
+
+    public double max() {
         return 0;
     }
-    
-    public double findTempClosestToValue(double tempValue){
+
+    public double findTempClosestToZero() {
         return 0;
-}
-    
-    public double[] findTempsLessThen(double tempValue){
+    }
+
+    public double findTempClosestToValue(double tempValue) {
+        return 0;
+    }
+
+    public double[] findTempsLessThen(double tempValue) {
         return null;
     }
-    
-    public double[] findTempsGreaterThen(double tempValue){
+
+    public double[] findTempsGreaterThen(double tempValue) {
         return null;
     }
-    
-    public TempSummaryStatistics summaryStatistics(){
+
+    public TempSummaryStatistics summaryStatistics() {
         return null;
     }
-    
-    public int addTemps(double ... temps){
+
+    public int addTemps(double... temps) {
         return 0;
     }
 }
